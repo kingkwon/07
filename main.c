631b725c30f7b6f4ab3e1579346dc0e5bc12a8e0
kingkwon <55428422+kingkwon@users.noreply.github.com>
2020-10-16 01:09:44 +0900

    실습 07

--- main.c
+++ main.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-static int all_files;
-extern void sub();
-
-int main(void)
+int factorial(int n)
 {
-	sub();
-	printf("%d\n", all_files);
+	if (n <= 1) return(1);
+	else return (n*factorial(n-1));
+}
+int main (void)
+{
+	int n, result;
+	
+	printf("inter a integer : ");
+	scanf("%d", &n);
+	
+	result = factorial(n);
+	printf("%d", result);
+	
 	return 0;
 }
 
