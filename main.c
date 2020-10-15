02e501a8470909d1e89af880f6288f05358a1f85
kingkwon <55428422+kingkwon@users.noreply.github.com>
2020-10-15 15:35:06 +0900

    실습 01

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int flag =1;
+	int y; //y선언을 중괄호 밖에서 하거나, 중괄호  밖에서 y를 쓰지 않으면 됨 
+	
+	while(flag != 0)
+	{
+		y =3;
+		flag =0;
+	}
+	y=4;
+	
+	return 0;
+}
